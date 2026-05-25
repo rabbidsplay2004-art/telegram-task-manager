4cbe29cd1ba6eed8f2fa835589e85a23e9131eae
rabbidsplay2004-art <rabbidsplay2004@gmail.com>
2026-05-25 18:17:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ from aiogram.types import (
     InlineKeyboardButton,
     CallbackQuery
 )
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,7 +20,16 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+# Временное хранилище задач
+tasks = []
 
+
+# FSM
+class CreateTask(StatesGroup):
+    waiting_for_title = State()
+
+
+# Главное меню
 def main_menu():
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
@@ -72,11 +83,58 @@ async def start(message: Message):
     )
 
 
+# Создание задачи
+@dp.callback_query(F.data == "new_task")
+async def new_task(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer(
+        "✍️ Введите название задачи"
+    )
+
+    await state.set_state(CreateTask.waiting_for_title)
+
+
+# Получение названия задачи
+@dp.message(CreateTask.waiting_for_title)
+async def get_task_title(message: Message, state: FSMContext):
+    task = {
+        "title": message.text,
+        "user_id": message.from_user.id
+    }
+
+    tasks.append(task)
+
+    await message.answer(
+        f"✅ Задача создана:\n\n📌 {message.text}",
+        reply_markup=main_menu()
+    )
+
+    await state.clear()
+
+
+# Мои задачи
 @dp.callback_query(F.data == "my_tasks")
 async def my_tasks(callback: CallbackQuery):
-    await callback.message.answer("📋 Здесь будут ваши задачи")
+
+    user_tasks = [
+        task for task in tasks
+        if task["user_id"] == callback.from_user.id
+    ]
+
+    if not user_tasks:
+        await callback.message.answer(
+            "📭 У вас пока нет задач"
+        )
+        return
+
+    text = "📋 Ваши задачи:\n\n"
+
+    for i, task in enumerate(user_tasks, start=1):
+        text += f"{i}. {task['title']}\n"
+
+    await callback.message.answer(text)
 
 
+# Остальные кнопки
 @dp.callback_query(F.data == "today")
 async def today(callback: CallbackQuery):
     await callback.message.answer("📅 Задачи на сегодня")
@@ -85,11 +143,6 @@ async def today(callback: CallbackQuery):
 @dp.callback_query(F.data == "all_tasks")
 async def all_tasks(callback: CallbackQuery):
     await callback.message.answer("📊 Все задачи")
-
-
-@dp.callback_query(F.data == "new_task")
-async def new_task(callback: CallbackQuery):
-    await callback.message.answer("➕ Создание новой задачи")
 
 
 @dp.callback_query(F.data == "overdue")
